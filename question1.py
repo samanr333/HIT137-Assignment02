3d5756ad8a9ead90e235e78aa47f7c6e45ec73f2
samanr333 <samanraut.sr@gmail.com>
2026-01-07 15:34:48 +1100

    chore: initialisation of question no 1

---
+++ question1.py
@@ -0,0 +1,4 @@
+# Create a program that reads the text file "raw_text.txt", encrypts its contents using a
+# simple encryption method, and writes the encrypted text to a new file
+# "encrypted_text.txt". Then create a function to decrypt the content and a function to
+# verify the decryption was successful.
