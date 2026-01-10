bd995e678a35e96097cd120c846b5d0f81a257f3
samanr333 <samanraut.sr@gmail.com>
2026-01-10 18:55:26 +1100

    chore: Decryption function

--- question1.py
+++ question1.py
@@ -2,3 +2,60 @@
 # simple encryption method, and writes the encrypted text to a new file
 # "encrypted_text.txt". Then create a function to decrypt the content and a function to
 # verify the decryption was successful.
+
+# Encryption function
+def encryption(shift1, shift2):
+    # Reading data from raw_text.txt file
+    with open("raw_text.txt", "r") as f:
+        input_txt = f.read()
+    # Initialising a string that stores the encrypted text
+    encrypted_text = ""
+
+    # Encryption logic
+    # The ord() function takes the character and converts it to its corresponding ASCII value
+    # The chr() function takes ASCII value and converts it to corresponding character
+    for ch in input_txt:
+        if ch.islower():
+            if ch <= 'm':
+                encrypted_text += chr((ord(ch) - 97 + (shift1 * shift2)) % 26 + 97)
+            else:
+                encrypted_text += chr((ord(ch) - 97 - (shift1 + shift2)) % 26 + 97)
+
+        elif ch.isupper():
+            if ch <= 'M':
+                encrypted_text += chr((ord(ch) - 65 - shift1) % 26 + 65)
+            else:
+                encrypted_text += chr((ord(ch) - 65 + (shift2 * shift2)) % 26 + 65)
+
+        else:
+            encrypted_text += ch
+    # Creating a text file with the encrypted text
+    with open("encrypted_text.txt", "w") as f:
+        f.write(encrypted_text)
+
+# Decryption function
+def decryption(shift1, shift2):
+    with open("encrypted_text.txt", "r") as f:
+        input_txt = f.read()
+
+    decrypted_text = ""
+    for ch in input_txt:
+        if ch.islower():
+            if ch <= 'm':
+                decrypted_text += chr((ord(ch) - 97 - (shift1 * shift2)) % 26 + 97)
+            else:
+                decrypted_text += chr((ord(ch) - 97 + (shift1 + shift2)) % 26 + 97)
+
+        elif ch.isupper():
+            if ch <= 'M':
+                decrypted_text += chr((ord(ch) - 65 + shift1) % 26 + 65)
+            else:
+                decrypted_text += chr((ord(ch) - 65 - (shift2 * shift2)) % 26 + 65)
+
+        else:
+            decrypted_text += ch
+
+    with open("decrypted_text.txt", "w") as f:
+        f.write(decrypted_text)
+
+    return decrypted_text
